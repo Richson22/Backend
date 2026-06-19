e6cf6cc72ba949ba2243f31cd8f61179a859fe9d
Richson22 <abachorsimon2021@gmail.com>
2026-06-19 15:03:54 +0100

    admin login issues

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -15,6 +15,10 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  family: 4,
 });
 
 function generateOtp() {
